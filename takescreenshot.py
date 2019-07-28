a89b5756a98ad2f1ea0f0d03a3c2e1443966c93e
Ayan Banerjee <ayanb07@gmail.com>
2019-07-28 18:24:44 +0530

    Changed Weight factor added debug code

---
+++ takescreenshot.py
@@ -0,0 +1,6 @@
+import webscreenshot
+
+#webscreenshot.take_webscreenshot("https://twitter.com/ShashiTharoor?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor", "twitterShashiTharoor.png")
+#webscreenshot.take_webscreenshot("https://timesofindia.indiatimes.com/india/mamata-banerjees-appeasement-policy-killed-bengals-peace-outgoing-governor-kn-tripathi/articleshow/70415430.cms", "timesMamata.png")
+#webscreenshot.take_webscreenshot("https://www.ndtv.com/india-news/kargil-diwas-wont-come-under-any-pressure-when-it-comes-to-national-security-pm-modi-2076371", "ndtvModi.png")
+webscreenshot.take_webscreenshot("https://github.com/ayanb07", "gitHubAyan.png")
--- webscreenshot.py
+++ webscreenshot.py
@@ -12,20 +12,31 @@ driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, chrome_options=opt
 def score( array ):
 	return scipy.stats.rankdata(array)
 
+def my_debug( images, feature ):
+	#print( map(return_image_sting, images) )
+	print( feature )
+	print( score(feature) )
+
 def take_webscreenshot(url, imagename):
 	driver.get(url)
 	driver.maximize_window()
 	images = driver.find_elements_by_tag_name("img")
 	view_port = driver.get_window_size()
+	print("View port "+str(view_port))
 	h_view_port = view_port['height']
 	w_view_port = view_port['width']
 	view_port_area = h_view_port*w_view_port
+	print("View port area "+str(view_port_area))
 	image_size = []
 	aspect_ratio1 = []#16/9 or 4/3
 	aspect_ratio2 = []#16/9 or 4/3
 	position = []
-	area_ratio = []
+	height_ratio = []
+	width_ratio = []
 	weight = []
+	#aspect_ratio weighting factor all feature weighting factor [image_size=0.15, aspect_ration=0.05, position= 0.45, area_ration=0.35]
+	weight_factor = [0.15, 0.05, 0.45, 0.35]
+	#area_ration convert product to sum
 	for image in images:
 		h = image.rect['height']+0.001
 		w = image.rect['width']+0.001
@@ -35,18 +46,52 @@ def take_webscreenshot(url, imagename):
 		image_size.append(h*w)
 		aspect_ratio1.append( 1/(0.001+np.abs(16.0/9.0 - float(np.maximum(h,w))/float(np.minimum(h,w)))) )#16/9 or 4/3
 		aspect_ratio2.append( 1/(0.001+np.abs(4.0/3.0 - float(np.maximum(h,w))/float(np.minimum(h,w)))) )#16/9 or 4/3
-		position.append(-y)
-		area_ratio.append( float(h*w)/float(view_port_area) )
-	weight = np.add( weight, score(image_size) )
+		if y < 0:
+			y = y % h_view_port # this is to handle images above view port : bring them from below
+		position.append(-y)#append -y to make image coming first more dominant
+		height_ratio.append( float(h)/float(h_view_port) )
+		width_ratio.addend( float(w)/float(w_view_port) )
+	# image_size [ 100, 10000, 150000 ]
+	# aspect_ratio1 [ 1/0.1 1/0.001]
+	weight1 = weight_factor[0]*score(image_size)
+	#my_debug( images, image_size )
 	aspect_ratio = np.maximum(aspect_ratio1, aspect_ratio2)
-	weight = np.add( weight, score(aspect_ratio) )
-	weight = np.add( weight, score(position) )
-	weight = np.add( weight, score(area_ratio) )
-	print(weight)
-	if len(weight) == 0 :
+	weight2 = weight_factor[1]*score(aspect_ratio) 
+	weight3 = weight_factor[2]*score(position)
+	weight4 = weight_factor[3]*np.add( score(height_ratio), score(width_ratio) )
+	#print(weight)
+	weight = np.add(weight, weight1)
+	weight = np.add(weight, weight2)
+	weight = np.add(weight, weight3)
+	weight = np.add(weight, weight4)
+	#weight = np.add( weight1, weight2, weight3, weight4 )
+	#Debug start
+	i=0
+	for image in images:
+		src = image.get_attribute('src')
+		print("Current Image URL\n")
+		print(src)
+		print(image.rect)
+		print(image_size[i])
+		print(weight1[i])
+		print(aspect_ratio1[i])
+		print(aspect_ratio2[i])
+		print(weight2[i])
+		print(position[i])
+		print(weight3[i])
+		print(area_ratio[i])
+		print(weight4[i])
+		i=i+1
+	#Debug End
+	max_index = np.argmax(weight)
+	print("Dominant Image aread_ration : "+str(area_ratio[max_index]))
+	print("Dominant Image rect "+ str(images[max_index].rect))
+	src = images[max_index].get_attribute('src')
+	print("Dominant Image URL\n")
+	print(src)
+	if len(weight) == 0 or height_ratio[max_index] < 0.075 or width_ratio[max_index]< 0.075:
 		driver.save_screenshot(imagename)
 	else:
-		max_index = np.argmax(weight)
 		print("Before Test : "+ str(max_index) +"\n")
 		src = images[max_index].get_attribute('src')
 		print("Test\n")
