b86e9f1ecb51af0831b23092b3ecc8b27cad12a9
Ayan Banerjee <ayanb07@gmail.com>
2019-08-04 21:56:50 +0530

    Logfile name by date time

--- logapi.py
+++ logapi.py
@@ -1,9 +1,11 @@
 #importing module 
 import logging
+from datetime import datetime
 #Create and configure logger 
-logging.basicConfig(filename="newfile.log", 
+log_file_name = datetime.now().strftime('image_generation_logfile_%H_%M_%d_%m_%Y.log')
+logging.basicConfig(filename=log_file_name, 
                     format='%(asctime)s %(message)s', 
-                    filemode='w')
+                    filemode='a')
 #Creating an object
 logger=logging.getLogger(__name__)
 #Setting the threshold of logger to DEBUG 
